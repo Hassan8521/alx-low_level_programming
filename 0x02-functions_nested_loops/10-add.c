042d1cea4f65c6bf34be90c78de92f88b0b32c19
Hassan8521 <laazizhassan11@gmail.com>
2022-03-16 13:24:26 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int add(int a, int b)
+{
+return (a + b);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Statu
