2b95097d43e00c80c7618a64ed18d98169942647
Hassan8521 <laazizhassan11@gmail.com>
2022-03-21 15:16:01 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strlen - return len of a sring
+*
+*@s: points at string
+*Return: always 0
+*/
+void _strlen(char *s)
+{
+len = _strlen(char *s)
+}
