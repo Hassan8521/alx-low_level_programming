cde35dc0a2424ce82f5ce1fca246d0b97f698f50
Hassan8521 <laazihassan8521>
2022-04-06 04:16:25 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,37 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * str_concat - concatenates two string
- *
- * @s1: the first string
- * @s2: the string to add to @s1
- *
- * Return: a pointer that points to a newly allocated space which
- * contains the contents of @s1, followed by the contents of @s2,
- * and null terminated. Should return NULL on failure
+ * str_concat - concatenates two strs
+ * @s1: str 1
+ * @s2: str 2
+ * Return: pointer to memory with concatenated str
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int s1len = 0;
-	unsigned int s2len = 0;
-	unsigned int s3len;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	char *p;
+	int i, j, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[s1len])
-		s1len++;
-	while (s2[s2len])
-		s2len++;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	s3len = s1len + s2len;
-
-	s3 = malloc(sizeof(char) * s3len + 1);
-	if (s3 == NULL)
+	p = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (p == NULL)
 		return (NULL);
 
-	while (i < s1len)
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		s3[i] = s1[i];
-		i++;
+		if (c < i)
+			p[c] = s1[c];
+		else
+			p[c] = s2[d++];
 	}
 
-	while (i <= s3len)
-	{
-		s3[i] = s2[j];
-		i++;
-		j++;
-	}
-	return (s3);
+	return (p);
 }
